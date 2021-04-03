84ff25f9710d7b5199571685c7662045c8a0c782
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-04-03 10:27:30 +0530

    Added meta information

--- discord/ext/wizard/__init__.py
+++ discord/ext/wizard/__init__.py
@@ -1,2 +1,13 @@
+from collections import namedtuple
+
+_VersionInfo = namedtuple("_VersionInfo", "major minor patch")
+version_info = _VersionInfo(0, 1, 0)
+
+__version__ = ".".join(version_info)
+__license__ = "MIT"
+__author__ = "FalseDev"
+__title__ = "discord-ext-wizard"
+__copyright__ = "Copyright 2021 {}".format(__author__)
+
 from .prompt import Prompt
 from .wizard import EmbedWizard, WizardFailure
